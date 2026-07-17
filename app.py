6dbee44f834308c045afd132dc8fbd2cb4bbf9b2
parveenshakya001 <parveenshakya969@gmail.com>
2026-07-17 13:06:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ interface = gr.Interface(
     ],
     outputs=gr.Textbox(label="Assessment Result"),
     title="Diabetes Prediction System",
-    description="Enter the medical details to predict diabetes risk using a Decision Tree model.",
+    description="Enter the medical details to predict diabetes risk using a Decision Tree model.\n Parveen :- 252152",
 )
 
 
